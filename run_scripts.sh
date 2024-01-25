22daa0fc04d1d5a39614b873f1581735723af285
MARK SANDOMENO <marksando11@gmail.com>
2024-01-24 22:35:40 -0500

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Run the first Python script
+
 echo "Running the first script (scraping)..."
 python3 scrape.py
 
-# Run the second Python script
+
 echo "Running the second script (extracting text)..."
 python3 htmlextract.py
 
